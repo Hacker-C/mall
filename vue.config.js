6615f089440d832eef532560f54a87dfb98c3cb8
Hacker-C <1507559148@qq.com>
2022-04-21 22:53:07 +0800

    🔧 移动端适配配置

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias: {
-        assets: '@/assets',
-        components: '@/components',
-        common: '@/common',
-        apis: '@/apis',
-        views: '@/views'
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          require('postcss-pxtorem')({
+            // 把px单位换算成rem单位
+            rootValue: 37.5, // vant官方使用的是37.5
+            selectorBlackList: ['vant', 'mu'], // 忽略转换正则匹配项
+            propList: ['*']
+          })
+        ]
       }
     }
   }
