20f4cdeefa4625d76b0b74598b59c7cf5fa9b0a3
Hacker-C <1507559148@qq.com>
2022-04-22 23:26:29 +0800

    ✨ feat: 本周流行完成

---
+++ src/components/content/home/components/FeaturesView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <a href="#">
+      <img src="@/assets/images/home/recommend_bg.jpg" alt="" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FeaturesView'
+}
+</script>
+
+<style lang="less" scoped>
+a img {
+  width: 100%;
+}
+</style>
