b26c948c826f3420f3eb8650e73704eb229a6c9f
Hacker-C <1507559148@qq.com>
2022-04-22 12:52:20 +0800

    🔧 配置 style-resources-loader 依赖

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,18 @@ module.exports = {
         ]
       }
     }
+  },
+  chainWebpack: config => {
+    const oneOfsMap = config.module.rule('less').oneOfs.store
+    oneOfsMap.forEach(item => {
+      item
+        .use('style-resources-loader')
+        .loader('style-resources-loader')
+        .options({
+          // or an array : ["./path/to/vars.less", "./path/to/mixins.less"] 这里的路径不能使用@，否则会报错
+          patterns: './src/assets/css/base.less'
+        })
+        .end()
+    })
   }
 }
