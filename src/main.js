ff66f94423a5f2e539c55a75cdb915465baa5f99
Hacker-C <1507559148@qq.com>
2022-04-22 12:51:29 +0800

    🐛 fix: 修复less全局变量失效问题

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router/index.js'
+import router from './router'
 import 'amfe-flexible'
+import './assets/css/base.less'
 
 Vue.config.productionTip = false
 
