fd8428e4dc37be2212fc3afffb4ab44e80e0648d
Hacker-C <1507559148@qq.com>
2022-04-23 16:10:47 +0800

    💄 style: 基本布局完成

---
+++ src/components/content/goods/GoodsItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="goods-item">
+    <img :src="item.show.img" />
+    <p>{{ item.title }}</p>
+    <div class="info">
+      <span>{{ price(item.orgPrice) }}</span>
+      <span>
+        <img class="star" src="@/assets/icons/star.png" alt="" />
+        {{ item.cfav }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GoodsItem',
+  props: {
+    item: {
+      type: Object
+    }
+  },
+  methods: {
+    price(p) {
+      return p.slice(1)
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.goods-item {
+  width: 48%;
+  img {
+    width: 100%;
+  }
+  p {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-size: 18px;
+    padding: 5px;
+    color: #333;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .info {
+    display: flex;
+    justify-content: center;
+    span:nth-child(1) {
+      color: @color-tint;
+      margin-right: 5px;
+    }
+    font-size: 20px;
+  }
+  .star {
+    width: 23px;
+    vertical-align: middle;
+  }
+}
+</style>
---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="goods-list">
+    <GoodsItem v-for="item in goodsList" :key="item.image" :item="item" />
+  </div>
+</template>
+
+<script>
+import GoodsItem from './GoodsItem.vue'
+export default {
+  name: 'GoodsList',
+  props: {
+    goodsList: {
+      type: Array
+    }
+  },
+  components: {
+    GoodsItem
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.goods-list {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+</style>
