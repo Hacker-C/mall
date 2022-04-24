e5c50b3c1acfd97d35ea4ad13d9882007c36ba67
Hacker-C <1507559148@qq.com>
2022-04-24 15:12:07 +0800

    ✨ feat: 回到顶部完成

---
+++ src/components/content/backtop/BackTop.vue
@@ -0,0 +1,19 @@
+<template>
+  <img src="@/assets/images/common/top.png" alt="" />
+</template>
+
+<script>
+export default {
+  name: 'BackTop'
+}
+</script>
+
+<style lang="less" scoped>
+img {
+  width: 45px;
+  position: fixed;
+  right: 10px;
+  bottom: 80px;
+  z-index: 999;
+}
+</style>
