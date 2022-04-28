8acf669869ae30f7178f29df5417c0489ed848b4
Hacker-C <1507559148@qq.com>
2022-04-28 16:17:31 +0800

    ➕ 添加 vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import 'amfe-flexible'
 import './assets/css/base.less'
 
@@ -11,5 +12,6 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount('#app')
