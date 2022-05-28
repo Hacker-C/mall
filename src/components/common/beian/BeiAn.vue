98faaff4775488d1f08732bced57feadf2745566
Hacker-C <1507559148@qq.com>
2022-05-28 11:48:01 +0800

    💄 添加备案号信息

---
+++ src/components/common/beian/BeiAn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="beian">
+    ©MurphyChen 2022 |
+    <a href="https://beian.miit.gov.cn/" target="_blank">湘ICP备 2022004296号</a>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'BeiAn'
+}
+</script>
+
+<style scoped lang="less">
+.beian {
+  position: absolute;
+  bottom: 0;
+  display: block;
+  width: 100%;
+  font-size: 14px;
+  text-align: center;
+  color: #333;
+  background-color: #f5f4f5;
+  a {
+    color: #555;
+    text-decoration: none;
+  }
+}
+</style>
--- src/components/common/tabbar/TabBar.vue
+++ src/components/common/tabbar/TabBar.vue
@@ -1,67 +1,57 @@
 <template>
-  <div class="tabbar">
-    <TabBarItem link="/home">
-      <template v-slot:icon>
-        <img
-          v-if="$route.path === '/home'"
-          src="@/assets/icons/home_active.png"
-          alt=""
-        />
-        <img v-else src="@/assets/icons/home.png" alt="" />
-      </template>
-      <template v-slot:text>
-        <span>主页</span>
-      </template>
-    </TabBarItem>
-    <TabBarItem link="/category">
-      <template v-slot:icon>
-        <img
-          v-if="$route.path === '/category'"
-          src="@/assets/icons/classify_active.png"
-          alt=""
-        />
-        <img v-else src="@/assets/icons/classify.png" alt="" />
-      </template>
-      <template v-slot:text>
-        <span>分类</span>
-      </template>
-    </TabBarItem>
-    <TabBarItem link="/cart">
-      <template v-slot:icon>
-        <img
-          v-if="$route.path === '/cart'"
-          src="@/assets/icons/cart_active.png"
-          alt=""
-        />
-        <img v-else src="@/assets/icons/cart.png" alt="" />
-      </template>
-      <template v-slot:text>
-        <span>购物车</span>
-      </template>
-    </TabBarItem>
-    <TabBarItem link="/profile">
-      <template v-slot:icon>
-        <img
-          v-if="$route.path === '/profile'"
-          src="@/assets/icons/my_active.png"
-          alt=""
-        />
-        <img v-else src="@/assets/icons/my.png" alt="" />
-      </template>
-      <template v-slot:text>
-        <span>我的</span>
-      </template>
-    </TabBarItem>
+  <div>
+    <div class="tabbar">
+      <TabBarItem link="/home">
+        <template v-slot:icon>
+          <img v-if="$route.path === '/home'" src="@/assets/icons/home_active.png" alt="" />
+          <img v-else src="@/assets/icons/home.png" alt="" />
+        </template>
+        <template v-slot:text>
+          <span>主页</span>
+        </template>
+      </TabBarItem>
+      <TabBarItem link="/category">
+        <template v-slot:icon>
+          <img v-if="$route.path === '/category'" src="@/assets/icons/classify_active.png" alt="" />
+          <img v-else src="@/assets/icons/classify.png" alt="" />
+        </template>
+        <template v-slot:text>
+          <span>分类</span>
+        </template>
+      </TabBarItem>
+      <TabBarItem link="/cart">
+        <template v-slot:icon>
+          <img v-if="$route.path === '/cart'" src="@/assets/icons/cart_active.png" alt="" />
+          <img v-else src="@/assets/icons/cart.png" alt="" />
+        </template>
+        <template v-slot:text>
+          <span>购物车</span>
+        </template>
+      </TabBarItem>
+      <TabBarItem link="/profile">
+        <template v-slot:icon>
+          <img v-if="$route.path === '/profile'" src="@/assets/icons/my_active.png" alt="" />
+          <img v-else src="@/assets/icons/my.png" alt="" />
+        </template>
+        <template v-slot:text>
+          <span>我的</span>
+        </template>
+      </TabBarItem>
+    </div>
+    <!-- 由于我的域名已备案，所以必须加这个备案号信息，不需要的直接删除这个 BeiAn 组件，然后修改下面 style 中的 bottom 值 为 -1px -->
+    <BeiAn />
   </div>
 </template>
 
 <script>
 import TabBarItem from './TabBarItem'
+import BeiAn from '@/components/common/beian/BeiAn'
 
 export default {
   name: 'TabBar',
   components: {
-    TabBarItem
+    TabBarItem,
+    BeiAn
   },
   methods: {
     goto() {
@@ -77,7 +67,8 @@ export default {
   height: 70px;
   width: 100%;
   position: fixed;
-  bottom: -1px;
+  // bottom: -1px;
+  bottom: 10px;
   border-top: 1px solid #ccc;
   display: flex;
 }
